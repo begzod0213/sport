2ce50bb65a7f5da00aff1fc1126820fcb372838a
begzod0213 <bekburiboyev0311@gmail.com>
2024-12-11 15:49:13 +0500

    mmm

--- index.js
+++ index.js
@@ -214,10 +214,9 @@ bot.on('message', async (msg) => {
 const app = express();
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
-//app.listen(PORT, () => {
-   // console.log(`Server is running on port ${PORT}`);
-//})
+})
 });
